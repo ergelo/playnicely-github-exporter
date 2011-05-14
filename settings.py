54d37624f4a4eab383d184581cf10993b76f7262
Bruno Panara <bruno.panara@gmail.com>
2011-05-14 20:57:21 +0100

    cleaned up files and added settings template

---
+++ settings.py
@@ -0,0 +1,8 @@
+playnicely_user = "USER"
+playnicely_password = "PASSWORD"
+
+github_user = "USER"
+github_token = "TOKEN"
+github_password ="PASSWORD"
+
+simon_encoded = "HACK" #encoded string for other users, to be added in auth dictionary
